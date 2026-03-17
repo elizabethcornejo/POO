6fff78796150d004c5276bf3d7ef9890b082880b
Teté <elizabethcornejo@liceovvh.cl>
2026-03-17 12:56:35 -0300

    termianr tarea

---
+++ 04_funciones_js/static/js/script.js
@@ -0,0 +1,13 @@
+/*Ejercicio 1: Lista de Asistencia (push)
+Contexto: Eres el profesor y estás pasando la lista. Cada alumno que dice "presente" debe ser anotado al final del registro.
+Crea un arreglo vacío: let asistencia = [];
+Función Principal: Crea registrarAlumno(). (Esta va en el onclick del botón).
+Captura el nombre escrito en el input.
+Usa .push() para meter a ese alumno al final del arreglo asistencia.
+Modifica el textContent del párrafo para mostrar: "Alumnos presentes: " seguido del arreglo.
+Limpia el input vaciando su .value.
+ */
+
+function asistencia(){
+    let asistencia = [];
+}
